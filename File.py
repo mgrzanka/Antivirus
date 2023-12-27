7c228082062db37c930092a6f832671c660db599
mgrzanka <01186969@pw.edu.pl>
2023-12-27 15:42:38 +0100

    real path protection

--- File.py
+++ File.py
@@ -4,7 +4,7 @@ import hashlib
 import chardet
 import sqlite3
 
-from Messages import SuccessMessage, PermissionErrorMessage, FailureMessage
+from Messages import SuccessMessage, FailureMessage
 
 class File:
     def __init__(self, path: str) -> None:
@@ -26,7 +26,8 @@ class File:
             pass
        
     def is_binary(self):
-        with open(self.path, 'rb') as file:
+        real_path = os.path.realpath(self.path)
+        with open(real_path, 'rb') as file:
             if b'\x00' in file.read(1024):
                 return False
             if b'#!' in file.read(1024):
@@ -77,11 +78,8 @@ class File:
             shutil.move(file_path, destination_path)
             new_file_path = os.path.join(destination_path, os.path.split(file_path)[1])
             os.chmod(new_file_path, 0o000)
-            success_message = SuccessMessage(file_path, new_file_path)
+            success_message = SuccessMessage(new_file_path)
             success_message.display_message()
-        except PermissionError:
-            permission_error_message = PermissionErrorMessage(file_path, new_file_path)
-            permission_error_message.display_message()
         except Exception:
             failure_message = FailureMessage(file_path, new_file_path)
             failure_message.display_message()
