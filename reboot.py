bcdedd45920e61124181420e6892b2fe90342ab5
mgrzanka <01186969@pw.edu.pl>
2023-12-28 17:54:55 +0100

    any user name

--- reboot.py
+++ reboot.py
@@ -1,5 +1,16 @@
 from messages_code.Messages import RebootMessage
+from configuration_code.get_user_name import get_user_name
+from configuration_code.JsonFile import JsonFile
+
+import os
 
 if __name__ == "__main__":
-    reboot_message = RebootMessage()
+    username = get_user_name()
+    user_path = os.path.join("/home", username)
+    main_folder = os.path.join(user_path, "Antivirus")
+    default_path = os.path.join(main_folder, 'settings.json')
+
+    json_file = JsonFile(default_path)
+
+    reboot_message = RebootMessage(user_path, json_file)
     reboot_message.display_message()
